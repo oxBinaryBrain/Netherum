16033face114f285c6a42239ec2576fd32aa810b
oxBinaryBrain <udayg6269@gmail.com>
2024-03-20 21:38:21 +0530

    creation and deployment of account

--- main.cs
+++ main.cs
@@ -11,3 +11,17 @@ class Program
     {
         // Connect to an Ethereum network
         var web3 = new Web3("https://ropsten.infura.io/v3/your-infura-api-key");
+
+        // Create a new account (replace with your private key)
+        var account = new Account("your-private-key");
+
+        // Unlock the account
+        await web3.Personal.UnlockAccount.SendRequestAsync(account.Address, "your-account-password", 120);
+
+        // Deploy a simple smart contract
+        var deployment = new ContractDeployment()
+        {
+            ByteCode = "6060604052341561000f57600080fd5b60ad8061001d6000396000f30060606040526000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff168063cfae32171461004e578063f8b2cb4f1461006b5761004a5b600080fd5b341561005957600080fd5b61006161008d565b6040518082815260200191505060405180910390f35b341561008457600080fd5b61008c6100a8565b6040518082815260200191505060405180910390f35b600060009054906101000a900460ff1681565b60008054905090565b6000600190505b90565b6000600490509056fea165627a7a7230582081f3f98b4299b4dc2411bb5c0a0cf032a0e504fa126fd129c45c9060e3e18770029",
+            Gas = BigInteger.Parse("4700000")
+        };
+
